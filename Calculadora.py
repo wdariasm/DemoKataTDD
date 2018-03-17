98198953950ca28096817f96e46a881843499de2
Wendell Arias <wdarias@unicesar.edu.co>
2018-03-16 22:12:06 -0500

    Paso 3.Se modifica el codigo para probar con dos numeros.(Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -3,5 +3,7 @@ class Calculadora:
     def Sumar(self,cadena):
         if cadena =="":
             return 0
+        elif "," in cadena:
+            return int(cadena[0]) + int(cadena[2])
         else:
             return int(cadena)
